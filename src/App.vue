269951a86e9694e9d95ff25bb510e14190d0742f
GwanHooPark <hooho.dev@gmail.com>
2020-04-28 18:48:43 +0900

    netlify 11

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home Home10</router-link> |
+      <router-link to="/">Home Home11</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
